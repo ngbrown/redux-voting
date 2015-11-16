a7473ac50a26f931bc2b905b4fb32d5c9654849a
Nathan Brown <nathan@nkbrown.us>
2015-11-16 13:53:17 -0700

    Dispatching Actions From React Components

--- voting-client/src/components/Voting.jsx
+++ voting-client/src/components/Voting.jsx
@@ -3,6 +3,7 @@ import PureRenderMixin from "react-addons-pure-render-mixin";
 import {connect} from "react-redux";
 import Winner from "./Winner";
 import Vote from "./Vote";
+import * as actionCreators from "../action_creators";
 
 // Pure component (react-redux "dumb")
 export const Voting = React.createClass({
@@ -22,10 +23,14 @@ export const Voting = React.createClass({
 
 function mapStateToProps(state) {
   return {
+    hasVoted: state.get("hasVoted"),
     pair: state.getIn(["vote", "pair"]),
     winner: state.get("winner")
   };
 }
 
 // Connected component (react-redux "smart")
-export const VotingContainer = connect(mapStateToProps)(Voting);
+export const VotingContainer = connect(
+  mapStateToProps,
+  actionCreators
+)(Voting);
