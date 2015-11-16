770e49292bd228416827bf3db6a2515e200b238c
Nathan Brown <nathan@nkbrown.us>
2015-11-16 16:27:16 -0700

    Sending Actions To The Server Using Redux Middleware

--- voting-client/src/remote_action_middleware.js
+++ voting-client/src/remote_action_middleware.js
@@ -2,7 +2,7 @@
 // a "next" callback. That function returns a third function that takes a Redux
 // action. The innermost function is where the middleware implementation will
 // actually go.
-export default store => next => action => {
-  console.log("in middleware", action);
+export default socket => store => next => action => {
+  socket.emit("action", action);
   return next(action);
 };
