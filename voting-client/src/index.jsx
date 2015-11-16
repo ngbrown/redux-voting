0da69dc653557dc117176d2d32597a9142d1308d
Nathan Brown <nathan@nkbrown.us>
2015-11-16 10:57:05 -0700

    Handling Routing

--- voting-client/src/index.jsx
+++ voting-client/src/index.jsx
@@ -1,13 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Router, {Route} from "react-router";
+import App from "./components/App";
 import Voting from "./components/Voting";
+import Results from "./components/Results";
 
-const pair = ["Trainspotting", "28 Days Later"];
+const routes = (
+  <Route component={App}>
+    <Route
+        component={Results}
+        path="/results"
+    />
+    <Route
+        component={Voting}
+        path="/"
+    />
+  </Route>
+);
 
 ReactDOM.render(
-  <Voting
-      pair={pair}
-      winner="Trainspotting"
-  />,
+  <Router>{routes}</Router>,
   document.getElementById("app")
 );
